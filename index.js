24f4fcc87299298f1fbe30d9c9aa3fca92a732e1
Marek Kobida <marek.kobida@gmail.com>
2023-01-19 01:08:18 +0100

    up

--- index.js
+++ index.js
@@ -18,29 +18,33 @@ const prettier = {
 };
 
 const react = {
+  'react/destructuring-assignment': 2,
   // https://github.com/jsx-eslint/eslint-plugin-react/blob/master/lib/rules/jsx-key.js
   'react/jsx-key': 2,
+  'react/jsx-no-useless-fragment': 2,
   // https://github.com/jsx-eslint/eslint-plugin-react/blob/master/lib/rules/jsx-sort-props.js
   'react/jsx-sort-props': 2,
+  'react/no-array-index-key': 2,
+  'react/no-multi-comp': 2,
 };
 
 const typescript = {
-  // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/src/rules/array-type.ts
+  // https://github.com/typescript-eslint/typescript-eslint/blob/main/packages/eslint-plugin/src/rules/array-type.ts
   '@typescript-eslint/array-type': 2,
-  // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/src/rules/consistent-type-assertion.ts
+  // https://github.com/typescript-eslint/typescript-eslint/blob/main/packages/eslint-plugin/src/rules/consistent-type-assertions.ts
   '@typescript-eslint/consistent-type-assertions': 2,
-  // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/src/rules/consistent-type-definitions.ts
+  // https://github.com/typescript-eslint/typescript-eslint/blob/main/packages/eslint-plugin/src/rules/consistent-type-definitions.ts
   '@typescript-eslint/consistent-type-definitions': 2,
-  // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/src/rules/consistent-type-imports.ts
+  // https://github.com/typescript-eslint/typescript-eslint/blob/main/packages/eslint-plugin/src/rules/consistent-type-imports.ts
   '@typescript-eslint/consistent-type-imports': 2,
-  // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/src/rules/no-explicit-any.ts
+  // https://github.com/typescript-eslint/typescript-eslint/blob/main/packages/eslint-plugin/src/rules/no-explicit-any.ts
   '@typescript-eslint/no-explicit-any': 1,
-  // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/src/rules/no-inferrable-types.ts
+  // https://github.com/typescript-eslint/typescript-eslint/blob/main/packages/eslint-plugin/src/rules/no-inferrable-types.ts
   '@typescript-eslint/no-inferrable-types': 1,
-  // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/src/rules/no-unnecessary-condition.ts
-  '@typescript-eslint/no-unnecessary-condition': 0,
-  // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/src/rules/sort-type-union-intersection-members.ts
-  '@typescript-eslint/sort-type-union-intersection-members': 2,
+  // https://github.com/typescript-eslint/typescript-eslint/blob/main/packages/eslint-plugin/src/rules/no-unnecessary-condition.ts
+  '@typescript-eslint/no-unnecessary-condition': 1,
+  // https://github.com/typescript-eslint/typescript-eslint/blob/main/packages/eslint-plugin/src/rules/sort-type-constituents.ts
+  '@typescript-eslint/sort-type-constituents': 2,
 };
 
 module.exports = {
