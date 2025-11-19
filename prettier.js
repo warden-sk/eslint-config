f3ceedc2647ffc25a3ff3827727920ebfb87bd51
Marek Kobida <marek.kobida@gmail.com>
2025-11-19 21:15:44 +0100

    normalized indentation in `prettier.js` and `typescript.js`

--- prettier.js
+++ prettier.js
@@ -1,17 +1,19 @@
 /*
  * Copyright 2025 Marek Kobida
  */
+
 const prettier = {
-    'arrow-body-style': 0,
-    'prefer-arrow-callback': 0,
-    'prettier/prettier': [
-        2,
-        {
-            arrowParens: 'avoid',
-            experimentalTernaries: true,
-            printWidth: 120,
-            singleQuote: true,
-        },
-    ],
+  'arrow-body-style': 0,
+  'prefer-arrow-callback': 0,
+  'prettier/prettier': [
+    2,
+    {
+      arrowParens: 'avoid',
+      experimentalTernaries: true,
+      printWidth: 120,
+      singleQuote: true,
+    },
+  ],
 };
+
 export default prettier;
--- typescript.js
+++ typescript.js
@@ -1,33 +1,35 @@
 /*
  * Copyright 2025 Marek Kobida
  */
+
 const typescript = {
-    '@typescript-eslint/array-type': 2,
-    '@typescript-eslint/await-thenable': 2,
-    '@typescript-eslint/consistent-generic-constructors': 2,
-    '@typescript-eslint/consistent-indexed-object-style': [2, 'index-signature'],
-    '@typescript-eslint/consistent-type-assertions': 2,
-    '@typescript-eslint/consistent-type-definitions': [2, 'type'],
-    '@typescript-eslint/consistent-type-exports': 2,
-    '@typescript-eslint/consistent-type-imports': 2,
-    '@typescript-eslint/dot-notation': 2,
-    '@typescript-eslint/explicit-function-return-type': [
-        1,
-        {
-            allowExpressions: true,
-        },
-    ],
-    '@typescript-eslint/no-confusing-non-null-assertion': 2,
-    // '@typescript-eslint/no-explicit-any': 2,
-    '@typescript-eslint/no-floating-promises': 1,
-    '@typescript-eslint/no-inferrable-types': 2,
-    '@typescript-eslint/no-unnecessary-condition': 2,
-    '@typescript-eslint/no-unnecessary-type-assertion': 2,
-    '@typescript-eslint/no-unnecessary-type-constraint': 2,
-    '@typescript-eslint/prefer-function-type': 2,
-    '@typescript-eslint/promise-function-async': 1,
-    '@typescript-eslint/return-await': 1,
-    'dot-notation': 0,
-    'no-return-await': 0,
+  '@typescript-eslint/array-type': 2,
+  '@typescript-eslint/await-thenable': 2,
+  '@typescript-eslint/consistent-generic-constructors': 2,
+  '@typescript-eslint/consistent-indexed-object-style': [2, 'index-signature'],
+  '@typescript-eslint/consistent-type-assertions': 2,
+  '@typescript-eslint/consistent-type-definitions': [2, 'type'],
+  '@typescript-eslint/consistent-type-exports': 2,
+  '@typescript-eslint/consistent-type-imports': 2,
+  '@typescript-eslint/dot-notation': 2,
+  '@typescript-eslint/explicit-function-return-type': [
+    1,
+    {
+      allowExpressions: true,
+    },
+  ],
+  '@typescript-eslint/no-confusing-non-null-assertion': 2,
+  // '@typescript-eslint/no-explicit-any': 2,
+  '@typescript-eslint/no-floating-promises': 1,
+  '@typescript-eslint/no-inferrable-types': 2,
+  '@typescript-eslint/no-unnecessary-condition': 2,
+  '@typescript-eslint/no-unnecessary-type-assertion': 2,
+  '@typescript-eslint/no-unnecessary-type-constraint': 2,
+  '@typescript-eslint/prefer-function-type': 2,
+  '@typescript-eslint/promise-function-async': 1,
+  '@typescript-eslint/return-await': 1,
+  'dot-notation': 0,
+  'no-return-await': 0,
 };
+
 export default typescript;
